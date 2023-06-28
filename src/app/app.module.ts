0b508c8b6826e85bb769fbcdcad153a46f8b1907
asmita18ashii <asmita18ashii@gmail.com>
2023-06-28 11:50:03 +0530

    holacracy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApperanceSettingComponent } from './component/modules/apperance-setting/apperance-setting.component';
-import { Card1Component } from './component/services/supportive_component/card1/card1.component';
-import { Card2Component } from './component/services/supportive_component/card2/card2.component';
+import { Card1Component } from './component/shared/supportive_component/card1/card1.component';
+import { Card2Component } from './component/shared/supportive_component/card2/card2.component';
 
 @NgModule({
   declarations: [
