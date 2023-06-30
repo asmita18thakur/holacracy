c17ce0bc88eefb739d00d0ca668c4f5a7de1ff7b
Adarsh Singh <singhadarsh1797@gmail.com>
2023-06-30 14:08:15 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ApperanceSettingComponent } from './component/modules/apperance-setting/apperance-setting.component';
+import { RatecardMessagecenterComponent } from './component/modules/ratecard-messagecenter/ratecard-messagecenter.component';
 
 const routes: Routes = [
   {
     path:'apperancesetting',
     component:ApperanceSettingComponent
-  }
+  },
+  {
+    path:'messagecenter',
+    component:RatecardMessagecenterComponent
+  },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ApperanceSettingComponent } from './component/modules/apperance-setting/apperance-setting.component';
 import { Card1Component } from './component/shared/supportive_component/card1/card1.component';
 import { Card2Component } from './component/shared/supportive_component/card2/card2.component';
+import { RateCardConfigLedgerComponent } from './component/modules/rate-card-config-ledger/rate-card-config-ledger.component';
+import { RatecardMessagecenterComponent } from './component/modules/ratecard-messagecenter/ratecard-messagecenter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ApperanceSettingComponent,
     Card1Component,
-    Card2Component
+    Card2Component,
+    RateCardConfigLedgerComponent,
+    RatecardMessagecenterComponent
   ],
   imports: [
     BrowserModule,
