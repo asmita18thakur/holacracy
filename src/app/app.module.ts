9f222c1c4574914e6a76883710e1de0a949a075d
gaian-naveenujuri <naveen.ujuri@gaiansolutions.com>
2023-06-30 11:18:47 +0530

    plan-card.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ApperanceSettingComponent } from './component/modules/apperance-setting/apperance-setting.component';
 import { Card1Component } from './component/shared/supportive_component/card1/card1.component';
 import { Card2Component } from './component/shared/supportive_component/card2/card2.component';
+import { PlanCardComponent } from './component/shared/supportive_component/plan-card/plan-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ApperanceSettingComponent,
     Card1Component,
-    Card2Component
+    Card2Component,
+    PlanCardComponent
   ],
   imports: [
     BrowserModule,
