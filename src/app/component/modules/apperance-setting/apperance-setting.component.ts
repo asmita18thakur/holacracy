95ee8654b6050a6859ea6f8475ff98605eac3376
asmita18ashii <asmita18ashii@gmail.com>
2023-07-01 18:29:36 +0530

    landing

--- src/app/component/modules/apperance-setting/apperance-setting.component.ts
+++ src/app/component/modules/apperance-setting/apperance-setting.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit,ViewChild, ElementRef, HostListener, OnInit } from '@angular/core';
 import { card2,card2_01 } from 'src/app/contants/temp';
 
 @Component({
@@ -7,11 +7,56 @@ import { card2,card2_01 } from 'src/app/contants/temp';
   styleUrls: ['./apperance-setting.component.scss']
 })
 export class ApperanceSettingComponent {
-  cards: any[] = card2;
+  cards: any[] =card2;
   cards_2:any[]=card2_01
 
-  constructor(){
+  activeSection: string="section1";
+  @ViewChild('containerElement', { static: true })  containerRef:any= ElementRef;
 
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.scrollToStart();
   }
-   
+
+  scrollToStart() {
+    const container = this.containerRef.nativeElement;
+    container.scrollLeft = 0;
+  }
+
+  scrollToSection(sectionId: string) {
+    const section = document.getElementById(sectionId);
+    this.activeSection=sectionId
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  //sidebar text highlight when scroll 
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const sections = this.elementRef.nativeElement.querySelectorAll('section[id^="section"]');
+
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight ) {
+        this.activeSection = section.id;
+        break;
+      }
+    }
+    console.log(this.activeSection)
+  }
+
+  isSectionActive(sectionId: string) {
+    return this.activeSection === sectionId;
+  }
+
 }
+   
+
